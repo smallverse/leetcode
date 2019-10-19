0d6ed53dffe6cee4cb9a854223fb3b3d99c5477a
翟俊峰 <zhaijunfeng@dijunfengdeiMac.local>
2019-10-19 13:56:19 +0800

    m

---
+++ 11.盛最多水的容器.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=java
+ *
+ * [11] 盛最多水的容器
+ */
+
+// @lc code=start
+class Solution {
+    public int maxArea(int[] height) {
+        for (int i = 0; i < height.length; i++) {
+            
+        }
+        return 0;
+    }
+}
+// @lc code=end
+
