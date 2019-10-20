99cd8c85341561e9a40b53628a3a6268887a8ca9
翟俊峰 <zhaijunfeng@dijunfengdeiMac.local>
2019-10-20 17:58:36 +0800

    m

--- Test.java
+++ Test.java
@@ -4,30 +4,7 @@ class Test {
         int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
         // int[] nums = { 2, 1 };
 
-        removeDuplicates(nums);
-    }
-
-    public static int removeDuplicates(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
-        int k = 0, count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != nums[k] && k + 1 <= nums.length - 1) {
-                nums[k] = nums[k + 1];
-
-            }
-            k++;
-
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
-        }
-
-        System.out.println("j:" + count);
-
-        return count;
+        // removeDuplicates(nums);
     }
 }
+
